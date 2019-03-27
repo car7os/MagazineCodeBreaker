3afcbb6ae4ad9f9d990f372fec0a0262e0c34778
root <carlosvilela.rj@gmail.com>
2019-03-27 20:12:15 -0300

    Estrutura do Código - comentários

--- app/src/main/java/br/com/codebreaker/magazinecodebreaker/MainActivity.java
+++ app/src/main/java/br/com/codebreaker/magazinecodebreaker/MainActivity.java
@@ -1,7 +1,27 @@
+//+---------------------------------------------------------------------+
+//|                                               MagazineCodeBreaker   |
+//|                                        Copyright 2019, CodeBreaker  |
+//|                                      http://www.codebreaker.com.br  |
+//|                                                                     |
+//| Referencias bibliograficas no final do arquivo.                     |
+//+---------------------------------------------------------------------+
+
+//+---------------------------------------------------------------------+
+//| Pacote / Diretório da Classe                                        |
+//+---------------------------------------------------------------------+
+
 package br.com.codebreaker.magazinecodebreaker;
+
+//+---------------------------------------------------------------------+
+//| Bibliotecas Necessárias                                             |
+//+---------------------------------------------------------------------+
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+//+---------------------------------------------------------------------+
+//| Classe Principal
+//+---------------------------------------------------------------------+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +29,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
+
+//+------------------------------------------------------------------------------------------------+
+//|                                                                    Referencias Bibliograficas  |
+//| WebView - Convertendo Site em Aplicativo, AndroidPro (Visualizado em 27/03/2019)               |
+//| https://www.androidpro.com.br/blog/desenvolvimento-android/webview-converter-site-aplicativo/
+//+------------------------------------------------------------------------------------------------+
