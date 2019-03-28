ec1870932eeb36831e1056bd6e6c3eada69578fa
root <carlosvilela.rj@gmail.com>
2019-03-27 23:17:33 -0300

    Funcionalidades - Acesso ao Site - tela cheia - botão voltar

--- app/src/main/java/br/com/codebreaker/magazinecodebreaker/MainActivity.java
+++ app/src/main/java/br/com/codebreaker/magazinecodebreaker/MainActivity.java
@@ -18,6 +18,9 @@ package br.com.codebreaker.magazinecodebreaker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 //+---------------------------------------------------------------------+
 //| Classe Principal
@@ -25,11 +28,42 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView navegadorWeb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        WebViewClientImpl webViewClient = new WebViewClientImpl(this);
+
+
+        // Variavel com a url do seu site
+        String enderecoSite = "https://www.magazinevoce.com.br/magazinecodebreaker/";
+
+        // configurando o WebView
+        navegadorWeb = (WebView) findViewById(R.id.webview);
+        navegadorWeb.setWebViewClient(webViewClient);
+        navegadorWeb.loadUrl(enderecoSite);
+        WebSettings configNavegador = navegadorWeb.getSettings();
+        configNavegador.setJavaScriptEnabled(true);
+        configNavegador.setAllowUniversalAccessFromFileURLs(true);
+        configNavegador.setAppCacheEnabled(true);
+
+
+
+
+    }
+
+    @Override
+    public boolean onKeyDown (int keyCode, KeyEvent event){
+
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && navegadorWeb.canGoBack()){
+            navegadorWeb.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
 
